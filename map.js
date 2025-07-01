a1fee51b34a29507f3de094cfc6be8a8e36bb6d7
jf-773 <jgfxvx93@gmail.com>
2025-07-01 13:26:56 +0100

    map set

--- map.js
+++ map.js
@@ -1,8 +1,47 @@
-function warn() {
-    alert("Haiiii")
-}
+var map = L.map('map').setView([51.574349, -1.310892], 17);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+// let marker = L.marker([51.5, -0.09]).addTo(map);
+// var circle = L.circle([51.508, -0.11], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(map);
+// var polygon = L.polygon([
+//     [51.509, -0.08],
+//     [51.503, -0.06],
+//     [51.51, -0.047]
+// ]).addTo(map);
 
-function test() {
-    var word = "Diamond"
-    alert("Here is ", word)
-}
+// var polygon = L.polygon([
+//     [51.509, -0.08],
+//     [51.503, -0.06],
+//     [51.51, -0.047]
+// ]).addTo(map);
+
+// var popup = L.popup()
+//     .setLatLng([51.513, -0.09])
+//     .setContent("I am a standalone popup.")
+//     .openOn(map);
+
+// function onMapClick(e) {
+//     alert("You clicked the map at " + e.latlng);
+// }
+
+// map.on('click', onMapClick);
+
+
+
+// var popup = L.popup();
+
+// function onMapClick(e) {
+//     popup
+//         .setLatLng(e.latlng)
+//         .setContent("You clicked the map at " + e.latlng.toString())
+//         .openOn(map);
+// }
+
+// map.on('click', onMapClick);
